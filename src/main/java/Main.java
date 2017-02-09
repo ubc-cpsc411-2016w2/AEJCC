c7b9cf12ba7673dfd5ed4a4072b3d961162ef997
Ronald Garcia <ron.garcia@gmail.com>
2017-02-09 09:08:43 -0800

    Rewrote Main as a REPL. Changed name of "Root" since a JavaCC parser can have many entrypoints.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,27 @@ import ca.ubc.cs411.aejcc.ast.AE;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to AE!\nType \"exit\" to end.");
+
         AEParser aep = new AEParser(System.in);
 
         try {
-            AE result = aep.Root();
-            System.out.println("AST: " + result);
-            System.out.println("Interp: " + result.interp());
+            AE result;
+            do {
+                System.out.print("> ");
+                result = aep.REPLCmd();
+                if (result != null) {
+                    System.out.println("AST: " + result);
+                    System.out.println("Interp: " + result.interp());
+                }
+            } while(result != null);
         }
-        catch(ParseException pe) {
-            System.out.println("exception: " + pe);
+        catch(Exception e) {
+            System.out.println("exception: " + e);
         }
+        catch(Error e) {
+            System.out.println("error: " + e);
+        }
+        System.out.println("Goodbye!");
     }
 }
